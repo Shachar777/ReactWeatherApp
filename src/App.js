56196d289ad0e45270e1eafb5c0260ca6912c56c
Shachar Levy <levyshachar32@gmail.com>
2025-03-13 14:17:59 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import { useEffect } from "react";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=5dac36117a64d2af1421292a8e499d90")
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error("Error fetching data:", error));
+  }, []);
+
+  return <h1>Check the console for weather data!</h1>;
 }
+
 
 export default App;
